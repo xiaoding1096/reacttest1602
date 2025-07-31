ce967c9fe8ad889eb978edad68b83946cd9ff035
xiaoding1096 <87075229+xiaoding1096@users.noreply.github.com>
2025-07-31 20:13:07 +0200

    finish update and delete book

--- src/services/api.ts
+++ src/services/api.ts
@@ -107,3 +107,19 @@ export const uploadFileApi = (fileImg: any, folder: string) => {
         }
     );
 }
+
+export const updateBookAPI = (
+    _id: string,
+    mainText: string, author: string,
+    price: number, quantity: number, category: string,
+    thumbnail: string, slider: string[]
+) => {
+    const urlBackend = `/api/v1/book/${_id}`;
+    return axios.put<IBackendRes<IRegister>>(urlBackend,
+        { mainText, author, price, quantity, category, thumbnail, slider })
+}
+
+export const deleteBookApi = (_id : string) => {
+    const urlBackend = `/api/v1/book/${_id}`
+    return axios.delete<IBackendRes<string>>(urlBackend)
+}
